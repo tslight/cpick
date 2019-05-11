43d79280211b90c85eb434c748c97c44ca340192
tslight <tslight@pm.com>
2019-05-11 17:03:57 +0100

    rev version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cpick",
-    version="0.0.4",
+    version="0.0.5",
     author="Toby Slight",
     author_email="tslight@pm.me",
     description="Curses List Picker",
