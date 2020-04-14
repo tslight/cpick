b85f87f93a70d00499031452f352400686d542fd
tslight <tslight@pm.com>
2020-04-14 23:20:40 +0100

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cpick",
-    version="0.1.0",
+    version="0.1.1",
     author="Toby Slight",
     author_email="tslight@pm.me",
     description="Curses List Picker",
